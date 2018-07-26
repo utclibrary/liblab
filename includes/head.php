fc6d4bffcf8bf66c971dd333ecdfe3c0294e622b
Steven D. Shelton <steven.d.shelton@gmail.com>
2018-07-26 14:28:46 -0400

    template - switched out GTM tag

--- includes/head.php
+++ includes/head.php
@@ -60,13 +60,13 @@ ini_set('display_errors', 1);
    <body class="utcms department simple">
 <!-- Data Layer for Google Tag Manager -->
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5SZRFQ"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TRJN3JP"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5SZRFQ');</script>
+})(window,document,'script','dataLayer','GTM-TRJN3JP');</script>
 <!-- End Google Tag Manager -->
 <div id="dev-environment" class="alert alert-info" role="alert" style="padding: .25em;text-align: center;margin-bottom: 0px;">
             | <strong>DEV</strong> environment | </div>
