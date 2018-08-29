f83083475c570f7ce442811da16f3c84a4c5bf65
Steven D. Shelton <steven-shelton@utc.edu>
2018-08-29 12:50:01 -0400

    minor tweaks on commments in get-orgsync-feed.php

--- orgsync/get-orgsync-rss-feed.php
+++ orgsync/get-orgsync-rss-feed.php
@@ -3,17 +3,17 @@
 / need to edit cron to run every 30 mintues
 / $ sudo crontab -e
 / ADD
-/ MAILTO={email} (ex: steven-shelton@utc.edu)
-/ {star}/30 * * * * /usr/bin/php /var/www/html/orgsync/get-orgsync-rss-feed.php
+/ MAILTO={email} (ex: MAILTO=libtech@utc.edu)
+/ {star}/30 * * * * /usr/bin/php /var/www/html/orgsync/get-orgsync-rss-feed.php (replace {star} with *)
 */
+// get mocsync feed and write to txt file
 $feedUrl2 ="https://mocsyncorgs.utc.edu/organization/utc-library/events.rss";
 $data2 = file_get_contents($feedUrl2);
 $file2 = fopen('/var/www/html/orgsync/orgsync-feed.txt',"w");
 echo fwrite($file2,$data2);
 fclose($file2);
 echo "<br />all:".md5_file('/var/www/html/orgsync/orgsync-feed.txt');
-// get blog alerts
-//generate all list
+// get blog alerts feed and write to txt file
 $feedUrlAlerts ="https://blog.utc.edu/library/category/alerts/feed/";
 $dataAlerts = file_get_contents($feedUrlAlerts);
 $fileAlerts = fopen('/var/www/html/orgsync/alerts-rss.txt',"w");
