08819310084b02afd0f9f1fe5d2aebb8e1f23f5e
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-05-02 16:19:10 -0400

    AtoZv2 jQ to fix click on popover to remain open

--- library/databases/index-v2.php
+++ library/databases/index-v2.php
@@ -641,6 +641,7 @@ Popper.Defaults.modifiers.computeStyle.gpuAcceleration = !(window.devicePixelRat
 $('body').on('click', function (e) {
         //did not click a popover toggle, or icon in popover toggle, or popover
         if ($(e.target).data('toggle') !== 'popover'
+            && (!$(e.target).parents().hasClass('popover'))
             && $(e.target).parents('[data-toggle=\"popover\"]').length === 0
             && $(e.target).parents('.popover.in').length === 0) {
             $('[data-toggle=\"popover\"]').popover('hide');
